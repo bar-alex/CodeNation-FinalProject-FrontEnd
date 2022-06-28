ba1f100935f53ed0c63149704e734272b2b5d05f
Leejjohno <leej67885@gmail.com>
2022-06-28 17:37:07 +0100

    features mapped

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import Achievements from "./components/Achievements";
+import Activities from "./components/Activities";
+import Challenges from "./components/Challenges";
+import Routes from "./components/Routes";
 
 function App() {
   return (
     <div>
       fitness app
+      <div>
+        <Achievements />
+        <Activities />
+        <Challenges />
+        <Routes />
+      </div>
     </div>
   );
 }
