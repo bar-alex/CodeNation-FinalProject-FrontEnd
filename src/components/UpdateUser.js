8114be1d69b1ff25b73856f8df4e1c2fa4b151aa
Leejjohno <leej67885@gmail.com>
2022-06-29 17:13:11 +0100

    some changes to code for merge

--- src/components/UpdateUser.js
+++ src/components/UpdateUser.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
 
 const UpdateUser = ( { user, token, setUser, } ) => {
-    const [username, setUsername] = useState();
+    const [fullName, setFullName] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
 
     const submitHandler = (e) => {
         e.preventDefault();
-        console.log("update user - ", username, email, password);
+        console.log("update user - ", fullName, email, password);
         console.log("ToDo: UpdateUser -> updateUser( {user}, token, setUser )")
     }
 
     return(
         <div>
             <form onSubmit={submitHandler}>
-                <input onChange={(e) => setUsername(e.target.value)} />
+                <input onChange={(e) => setFullName(e.target.value)} />
                 <input onChange={(e) => setEmail(e.target.value)} />
                 <input onChange={(e) => setPassword(e.target.value)} />
                 <button type="submit">Update Account</button>
