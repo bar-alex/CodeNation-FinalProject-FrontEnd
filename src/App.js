bbca92328a3878db22a9c680c9f19828d35c1a81
NBeshev <beshev.code@gmail.com>
2022-06-29 13:16:28 +0100

    charts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+//import React, { useState, useEffect } from 'react';
+import './App.css';
+import LineChartTwo from "./components/LineChartTwo";
 import Achievements from "./components/Achievements";
 import Activities from "./components/Activities";
 import Challenges from "./components/Challenges";
 import Routes from "./components/Routes";
 
-function App() {
+const App = () => {
   return (
     <div>
-      fitness app
+      Fitness App
       <div>
         <Achievements />
         <Activities />
         <Challenges />
         <Routes />
       </div>
+      <LineChartTwo />
     </div>
   );
 };
