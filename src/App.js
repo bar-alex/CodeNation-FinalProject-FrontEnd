b50e87f587c141b51ce1aaa47abe9b6d45511b60
affan-11 <affanarif4@gmail.com>
2022-06-30 13:07:44 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar"
+import './App.css';
 
 function App() {
   return (
     <div>
-      fitness app
+       <Navbar />
     </div>
   );
 }
