9e5221c0ee25ba8f9a4a0869b1152741b54918a2
moh-qureshi <h-qureshi95@hotmail.co.uk>
2022-07-04 10:51:03 +0100

    mohammed

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,9 +1,43 @@
+import styled from "styled-components";
+import { useLoadScript, GoogleMap, Marker, DirectionsRenderer, Circle, MarkerClusterer } from "@react-google-maps/api";
+import { useMemo } from "react";
+
+
 function Routes(properties) {
-    return (
-      <div>
-        Activity Routes mate
-      </div>
+  const center = useMemo(() => ({lat: 53.47, lng: -2.24}), [])
+  const options = useMemo(() => ({
+    disableDefaultUI: true,
+    clickableIcons: false
+  }), [])
+  
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyC4rF0twpG2XX77FJIYfQKVDN9IoevXLys",
+  })
+  if (!isLoaded) return <div>Loading...</div>
+  return (
+      <DivStyled>
+        <div className="container">
+          <div className="controls">
+            <h1>controls</h1>
+          </div>
+          <div className="map">
+            <GoogleMap zoom={13} center = { center } mapContainerClassName= "map-container" options={options}></GoogleMap>
+          </div>
+        </div>
+      </DivStyled>
     );
   }
+
+  const DivStyled = styled.div`
+    .map{
+      width: 100%;
+      height: 100vh;
+    }
+
+    .map-container{
+      width: 100%;
+      height: 100vh;
+    }
+  `
   
   export default Routes;
